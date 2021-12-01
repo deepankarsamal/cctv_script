703811cb720da12590dd2ea1c6e667355fa86c19
deepankarsamal <58949664+deepankarsamal@users.noreply.github.com>
2021-12-02 00:40:23 +0530

    added script

---
+++ cctv_script.js
@@ -0,0 +1,136 @@
+function cctv_hack(encrypt_type) {
+  function initialize() {
+    console.log("Initialzinig script_29......");
+    console.log("Rev_erse_Hash");
+
+    for (let i = 1000; i < 8000; i++) {
+      // program to generate random strings
+
+      // declare all characters
+      const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      function generateString(length) {
+        let result = " ";
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+          result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+          );
+        }
+
+        return result;
+      }
+
+      console.log(generateString(i));
+    }
+  }
+
+  initialize();
+
+  function step_2() {
+    buildTable = (spitTable) => {
+      function script_02() {
+        const readline = require("readline").createInterface({
+          input: process.stdin,
+          output: process.stdout,
+        });
+
+        readline.question(
+          "\nBackdoor Found.\nInitialize Metasploit ? (Y/N)",
+          (name) => {
+            console.log(`Starting Metasploit attack!`);
+            readline.close();
+          }
+        );
+      }
+
+      script_02();
+
+      setTimeout(() => {
+        console.log("No. Of Cameras Accessible: ");
+      }, 9000);
+
+      setTimeout(() => {
+        for (let i = 0; i < 20; i++) {
+          console.log(`Camera ${i}`);
+        }
+      }, 10000);
+    };
+
+    buildTable();
+  }
+
+  step_2();
+
+  function step_3() {
+    setTimeout(() => {
+      console.log("\nPossible Exploits: Overload Packets For 30000ms");
+      setTimeout(() => {
+        console.log("Accessing adminsys...");
+        initialize();
+      }, 2000);
+      setTimeout(() => {
+        console.log("\n \n \n Overloaded 10 Cameras");
+      }, 2500);
+      setTimeout(() => {
+        for (let i = 0; i < 20; i++) {
+            console.log(`
+                                                                                                                                             
+                                                                                                                                             
+            CCCCCCCCCCCCCHHHHHHHHH     HHHHHHHHHUUUUUUUU     UUUUUUUUTTTTTTTTTTTTTTTTTTTTTTTIIIIIIIIIIYYYYYYY       YYYYYYYEEEEEEEEEEEEEEEEEEEEEE
+         CCC::::::::::::CH:::::::H     H:::::::HU::::::U     U::::::UT:::::::::::::::::::::TI::::::::IY:::::Y       Y:::::YE::::::::::::::::::::E
+       CC:::::::::::::::CH:::::::H     H:::::::HU::::::U     U::::::UT:::::::::::::::::::::TI::::::::IY:::::Y       Y:::::YE::::::::::::::::::::E
+      C:::::CCCCCCCC::::CHH::::::H     H::::::HHUU:::::U     U:::::UUT:::::TT:::::::TT:::::TII::::::IIY::::::Y     Y::::::YEE::::::EEEEEEEEE::::E
+     C:::::C       CCCCCC  H:::::H     H:::::H   U:::::U     U:::::U TTTTTT  T:::::T  TTTTTT  I::::I  YYY:::::Y   Y:::::YYY  E:::::E       EEEEEE
+    C:::::C                H:::::H     H:::::H   U:::::D     D:::::U         T:::::T          I::::I     Y:::::Y Y:::::Y     E:::::E             
+    C:::::C                H::::::HHHHH::::::H   U:::::D     D:::::U         T:::::T          I::::I      Y:::::Y:::::Y      E::::::EEEEEEEEEE   
+    C:::::C                H:::::::::::::::::H   U:::::D     D:::::U         T:::::T          I::::I       Y:::::::::Y       E:::::::::::::::E   
+    C:::::C                H:::::::::::::::::H   U:::::D     D:::::U         T:::::T          I::::I        Y:::::::Y        E:::::::::::::::E   
+    C:::::C                H::::::HHHHH::::::H   U:::::D     D:::::U         T:::::T          I::::I         Y:::::Y         E::::::EEEEEEEEEE   
+    C:::::C                H:::::H     H:::::H   U:::::D     D:::::U         T:::::T          I::::I         Y:::::Y         E:::::E             
+     C:::::C       CCCCCC  H:::::H     H:::::H   U::::::U   U::::::U         T:::::T          I::::I         Y:::::Y         E:::::E       EEEEEE
+      C:::::CCCCCCCC::::CHH::::::H     H::::::HH U:::::::UUU:::::::U       TT:::::::TT      II::::::II       Y:::::Y       EE::::::EEEEEEEE:::::E
+       CC:::::::::::::::CH:::::::H     H:::::::H  UU:::::::::::::UU        T:::::::::T      I::::::::I    YYYY:::::YYYY    E::::::::::::::::::::E
+         CCC::::::::::::CH:::::::H     H:::::::H    UU:::::::::UU          T:::::::::T      I::::::::I    Y:::::::::::Y    E::::::::::::::::::::E
+            CCCCCCCCCCCCCHHHHHHHHH     HHHHHHHHH      UUUUUUUUU            TTTTTTTTTTT      IIIIIIIIII    YYYYYYYYYYYYY    EEEEEEEEEEEEEEEEEEEEEE
+                                                                                                                                                 
+                                                                                                                                                 
+                                                                                                                                                 
+                                                                                                                                                 
+                                                                                                                                                 
+                                                                                                                                                 
+                                                                                                                                                 
+    `);
+        }
+      }, 20000);
+      setTimeout(() => {
+        console.log(`
+        :::::::::     :::     :::::::::   ::::::::   ::::::::       :::::::::: :::    :::     :::     ::::    ::::       :::    :::     :::     ::::::::::: ::: ::: ::: 
+        :+:    :+:  :+: :+:   :+:    :+: :+:    :+: :+:    :+:      :+:        :+:    :+:   :+: :+:   +:+:+: :+:+:+      :+:    :+:   :+: :+:       :+:     :+: :+: :+: 
+        +:+    +:+ +:+   +:+  +:+    +:+ +:+        +:+    +:+      +:+         +:+  +:+   +:+   +:+  +:+ +:+:+ +:+      +:+    +:+  +:+   +:+      +:+     +:+ +:+ +:+ 
+        +#++:++#+ +#++:++#++: +#++:++#:  +#++:++#++ +#+    +:+      +#++:++#     +#++:+   +#++:++#++: +#+  +:+  +#+      +#++:++#++ +#++:++#++:     +#+     +#+ +#+ +#+ 
+        +#+       +#+     +#+ +#+    +#+        +#+ +#+    +#+      +#+         +#+  +#+  +#+     +#+ +#+       +#+      +#+    +#+ +#+     +#+     +#+     +#+ +#+ +#+ 
+        #+#       #+#     #+# #+#    #+# #+#    #+# #+#    #+#      #+#        #+#    #+# #+#     #+# #+#       #+#      #+#    #+# #+#     #+#     #+#                 
+        ###       ###     ### ###    ###  ########   ########       ########## ###    ### ###     ### ###       ###      ###    ### ###     ### ########### ### ### ### 
+        `);
+        console.log(`
+        ########     ###    ########  ##     ##    ###    ####    ##    ##    ###    ########     ##     ##  ######  
+        ##     ##   ## ##   ##     ## ##     ##   ## ##    ##     ##   ##    ## ##   ##     ##    ###   ### ##    ## 
+        ##     ##  ##   ##  ##     ## ##     ##  ##   ##   ##     ##  ##    ##   ##  ##     ##    #### #### ##       
+        ########  ##     ## ##     ## ######### ##     ##  ##     #####    ##     ## ########     ## ### ## ##       
+        ##        ######### ##     ## ##     ## #########  ##     ##  ##   ######### ##   ##      ##     ## ##       
+        ##        ##     ## ##     ## ##     ## ##     ##  ##     ##   ##  ##     ## ##    ##     ##     ## ##    ## 
+        ##        ##     ## ########  ##     ## ##     ## ####    ##    ## ##     ## ##     ##    ##     ##  ######  
+        `)
+      }, 20000);
+    }, 20000);
+  }
+
+  setTimeout(() => {
+    step_3();
+  }, 3000);
+}
+
+cctv_hack();
+
